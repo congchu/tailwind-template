884ef30f2407c02d9913f2b9b149b0573646639e
congchu <cookie00421@gmail.com>
2020-09-19 20:25:48 +0900

    Updates

---
+++ precache-manifest.8940a090e5042191ba706b582ef87618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef4142b262da41de2575a251f4c2451",
+    "url": "/tailwind-template/index.html"
+  },
+  {
+    "revision": "aac2b68e9ae0bf5c8d18",
+    "url": "/tailwind-template/static/css/main.99bc7762.chunk.css"
+  },
+  {
+    "revision": "a64588eb72c48eb064b5",
+    "url": "/tailwind-template/static/js/2.b4b2f7ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tailwind-template/static/js/2.b4b2f7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac2b68e9ae0bf5c8d18",
+    "url": "/tailwind-template/static/js/main.cf64a335.chunk.js"
+  },
+  {
+    "revision": "a2353f266715671d7a07",
+    "url": "/tailwind-template/static/js/runtime-main.f236f3a4.js"
+  }
+]);
